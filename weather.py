ee02a1b336dc8f54c85ae4bebaf28107147c6550
vasu <vasug49@iitkgp.ac.in>
2018-10-16 19:23:13 +0530

    Base commit

---
+++ weather.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.W8Xep99fgUQ")
+soup = BeautifulSoup(page.content,"html.parser")
+#print(soup.prettify())
+#print(list(soup.children))
+div=soup.find_all('div',id="seven-day-forecast-container")
+seven_day = list(div)[0]
+#print(list(div)[0])
+li = seven_day.find_all('li',class_="forecast-tombstone")
+tombstone = list(li)[0]
+p = tombstone.find_all('p',class_="temp temp-high")
+today = list(p)[0]
+print(today.get_text())
