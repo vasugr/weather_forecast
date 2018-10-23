fd916983836ed2e8f69b428282af4badc056bded
vasu <vasug49@iitkgp.ac.in>
2018-10-23 22:10:52 +0530

    added a city

--- weather.py
+++ weather.py
@@ -16,4 +16,23 @@ def scrapeit():
 
     print(today.get_text())
    
+def scrapeit2():
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=34.5232&lon=-117.2157#.W89OAo5fgUQ")
+    soup = BeautifulSoup(page.content,"html.parser")
+    #print(soup.prettify())
+    #print(list(soup.children))
+    div=soup.find_all('div',id="seven-day-forecast-container")
+    seven_day = list(div)[0]
+    #print(list(div)[0])
+
+    li = seven_day.find_all('li',class_="forecast-tombstone")
+    tombstone = list(li)[0]
+    p = tombstone.find_all('p',class_="temp temp-high")
+    today = list(p)[0]
+
+    print(today.get_text())
+
+#print("SAN FRANCISCO: ")
 scrapeit()
+#print("APPLE VALLEY: ")
+scrapeit2()
