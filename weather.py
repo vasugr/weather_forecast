42f2da150844ed46c1048b89eaa04a41577d1d7a
vasugr <vasug49@iitkgp.ac.in>
2021-10-03 12:34:42 +0530

    made it look better

--- weather.py
+++ weather.py
@@ -14,7 +14,7 @@ def scrapeit():
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
    
 def scrapeit2():
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=34.5232&lon=-117.2157#.W89OAo5fgUQ")
@@ -30,7 +30,7 @@ def scrapeit2():
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
     
 def scrapeit3():
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=35.1289&lon=-117.9856#.W89Q_Y5fgUQ")
@@ -46,7 +46,7 @@ def scrapeit3():
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
     
 def scrapeit4():
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=40.7146&lon=-74.0071#.XZNKc3EzYUM")
@@ -62,7 +62,7 @@ def scrapeit4():
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
 
  
 def scrapeit5():
@@ -75,7 +75,7 @@ def scrapeit5():
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
     
 def scrapeit6():#For Massachusetts
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=42.3657&lon=-71.1083#.XZc1T3X7Q-Y")
@@ -87,7 +87,7 @@ def scrapeit6():#For Massachusetts
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
     
 def scrapeit7():#For California City
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=35.1289&lon=-117.9856#.XZc4BnX7Q-Y")
@@ -99,7 +99,7 @@ def scrapeit7():#For California City
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
     
 def scrapeit8():#For Chicago
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=41.8843&lon=-87.6324#.XZc573X7Q-Y")
@@ -111,7 +111,7 @@ def scrapeit8():#For Chicago
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
 
-    print(today.get_text())
+    print("\t" + today.get_text())
 
 def scrapeit9():#For pHILADELPHIA
     page = requests.get("https://forecast.weather.gov/MapClick.php?lat=38.8182&lon=-76.1587")
@@ -122,10 +122,9 @@ def scrapeit9():#For pHILADELPHIA
     tombstone = list(li)[0]
     p = tombstone.find_all('p',class_="short-desc")
     today = list(p)[0]
-
-    print(today.get_text())
+    print("\t" + today.get_text())
     
-print("\nToday's temperature at :\n")
+print("\nWeather conditions later today at :\n")
 print("SAN FRANCISCO: ")
 scrapeit()
 print("APPLE VALLEY: ")
